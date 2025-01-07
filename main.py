5cfdddea577ad349387a1bf59c7b2d3925641ce0
glen <glen84_dr@hotmail.com>
2025-01-07 09:28:31 +0100

    chacking agaoin with falke8-html

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from views.main_view import main_page
 
-status = True
-while status is True:
-    status = main_page()
+def start_programme():
+    status = True
+    while status is True:
+        status = main_page()
 
-# ------
+start_programme()
